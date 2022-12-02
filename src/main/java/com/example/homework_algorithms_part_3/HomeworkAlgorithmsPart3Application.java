4fe9bfa0fc95a5bd14c63d55c057ea554a444786
zemscky <aleksei.zemscky@yandex.ru>
2022-12-02 15:03:26 +0300

    Initial commit

---
+++ src/main/java/com/example/homework_algorithms_part_3/HomeworkAlgorithmsPart3Application.java
@@ -0,0 +1,13 @@
+package com.example.homework_algorithms_part_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkAlgorithmsPart3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkAlgorithmsPart3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework_algorithms_part_3/HomeworkAlgorithmsPart3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework_algorithms_part_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkAlgorithmsPart3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
